641e56672a6a7056a30b986c32b75c60b41db845
合肥海图微电子有限公司 <49272093+haitusense@users.noreply.github.com>
2021-03-14 01:03:35 +0900

    Update mapcnv.js

--- MapConverter/mapcnv.js
+++ MapConverter/mapcnv.js
@@ -1,1 +1,37 @@
-null
+console.log("loaded mapconv 0.01");
+
+const mapconv;
+
+(()=>{
+  function test() {
+    console.log("test");
+  };
+  
+  mapconv.test = test;
+})();
+
+function loadScript(targetId) {
+  // Add element
+  let parent = document.getElementById(targetId);
+
+  let element1 = document.createElement("button");
+  element1.innerText = "Excel -&gt; Yaml";
+  // element1.idName = "runExcel2Yaml";  
+  element1.className = "ms-Button";
+  //element1.onclick = () => 
+  
+  let element2 = document.createElement("button");
+  element2.innerText = "Excel &lt;- Yaml";
+  element2.className = "ms-Button";
+  //element2.onclick = () => 
+  
+  parent.appendChild(element1);
+  parent.appendChild(element2);
+  
+  // load form CDN
+  $.getScript("https://cdnjs.cloudflare.com/ajax/libs/js-yaml/4.0.0/js-yaml.min.js", () => {
+    console.log("loaded js-yaml");
+  });
+}
+
+
