35ddd0c405ba3e51484cef1c255646e3b0d9f386
合肥海图微电子有限公司 <49272093+haitusense@users.noreply.github.com>
2021-03-14 03:57:53 +0900

    Update mermaid.js

--- mermaid/mermaid.js
+++ mermaid/mermaid.js
@@ -1,1 +1,59 @@
-null
+const loadScript = null;
+
+(() => {
+
+  loadScript = (targetId) => {
+    // Add element
+    let element = `
+    <body>
+    <pre><code class="language-mermaid">
+      graph LR
+        A(Planed) --> B
+        B[Process-1] --> C
+        C[Process-2] --> D[Stop]
+        C --> E[Done]
+        style B fill:#f9f
+    </code></pre>
+
+    <div class="language-mermaid">
+      gantt
+	      title Progress Diagram
+	      dateFormat  YYYY-MM-DD
+	      section Lot-1
+	        Process-1 : crit, a1, 2014-01-01, 60d
+	        Process-2 :       a2, after a1  , 20d
+	      section Lot-2
+	        Process-1 :       b1, 2014-03-01, 60d
+	        Process-2 :       b2, after b1, 20d
+    </div>
+    `;
+
+    $(targetId).append(element);
+
+    // load form CDN
+    $.getScript("https://cdnjs.cloudflare.com/ajax/libs/mermaid/8.0.0/mermaid.min.js", () => {
+      console.log("loaded mermaid");
+      let config = {
+        startOnLoad: true,
+        theme: "forest",
+        flowchart: {
+          useMaxWidth: false,
+          htmlLabels: true
+        }
+      };
+      mermaid.initialize(config);
+      mermaid.init(undefined, document.querySelectorAll(".language-mermaid"));
+    });
+  };
+
+  /** Default helper for invoking an action and handling errors. */
+  async function tryCatch(callback) {
+    try {
+      await callback();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  console.log("loaded mermaid");
+})();
