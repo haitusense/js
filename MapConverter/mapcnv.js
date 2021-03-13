bc1a2118a18cc89711c872547e5d527b8b4ace00
合肥海图微电子有限公司 <49272093+haitusense@users.noreply.github.com>
2021-03-14 01:49:08 +0900

    Update mapcnv.js

--- MapConverter/mapcnv.js
+++ MapConverter/mapcnv.js
@@ -1,7 +1,8 @@
-console.log("loaded mapconv 0.013");
+console.log("loaded mapconv 0.014");
 
 
 const mapconv =()=>{ };
+const loadScript =()=>{ };
 
 (()=>{
   function test() {
@@ -9,28 +10,42 @@ const mapconv =()=>{ };
   };
   
   mapconv.test = test;
+
+  /** Default helper for invoking an action and handling errors. */
+  async function tryCatch(callback) {
+    try {
+      await callback();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  loadScript = (targetId)=> {
+    // Add element
+    let template = `
+      <button id="runExcel2Yaml" class="ms-Button">
+        <span class="ms-Button-label">Excel -&gt; Yaml</span>
+      </button>
+      <button id="runYaml2Excel" class="ms-Button">
+        <span class="ms-Button-label">Excel &lt;- Yaml</span>
+      </button>
+      <textarea id="textareaYaml">
+      </textarea>
+    `;
+
+    //$(targetId).append(_.template(template, data));
+    $(targetId).append(template);
+    $("#runExcel2Yaml").click(() => tryCatch(runExcel2Yaml));
+    $("#runYaml2Excel").click(() => tryCatch(runYaml2Excel));
+    
+    // load form CDN
+    $.getScript("https://cdnjs.cloudflare.com/ajax/libs/js-yaml/4.0.0/js-yaml.min.js", () => {
+      console.log("loaded js-yaml");
+    });
+  }
+  
 })();
 
-function loadScript(targetId) {
-  // Add element
-  let template = `
-    <button id="runExcel2Yaml" class="ms-Button">
-      <span class="ms-Button-label">Excel -&gt; Yaml</span>
-    </button>
-    <button id="runYaml2Excel" class="ms-Button">
-      <span class="ms-Button-label">Excel &lt;- Yaml</span>
-    </button>
-    <textarea id="textareaYaml">
-    </textarea>
-  `;
 
-  //$(targetId).append(_.template(template, data));
-  $(targetId).append(template);
-
-  // load form CDN
-  $.getScript("https://cdnjs.cloudflare.com/ajax/libs/js-yaml/4.0.0/js-yaml.min.js", () => {
-    console.log("loaded js-yaml");
-  });
-}
 
 
