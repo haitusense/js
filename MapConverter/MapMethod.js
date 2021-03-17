d01d3ab363153b0a35a032d2b1a1862025754be6
合肥海图微电子有限公司 <49272093+haitusense@users.noreply.github.com>
2021-03-17 22:21:26 +0900

    Update MapMethod.js

--- MapConverter/MapMethod.js
+++ MapConverter/MapMethod.js
@@ -1,65 +1,62 @@
 define([],() => {
 
-  const MapMethod = {
-    bottom: 0,
-    left: 1,
-    top: 2,
-    right: 3,
+  const bottom = 0;
+  const left = 1;
+  const top = 2;
+  const right = 3;
 
-    toAscii(src: any[][], head = "") {
-      const blank = " ";
-      return src.reduce((acc_row, cur_row) => {
-        let hoge = cur_row.map((x) => (x === "" ? blank : String(x))).join("");
-        return `${acc_row}${head}${hoge}\r\n`;
-      }, "");
-    },
-
-    rotation(src: any[][], rot = 1) {
-      if (rot > 0) {
-        return MapMethod.rotation(MapMethod.rightRotation(src), rot - 1);
-      } else if (rot < 0) {
-        return MapMethod.rotation(MapMethod.leftRotation(src), rot + 1);
-      } else {
-        return src;
-      }
-    },
-    /*
-      Array(Math.abs(rot)).fill(0).forEach(i => {
-        if (rot > 0) {
-          ary = A.rightRotation(ary);
-        } else {
-          ary = A.leftRotation(ary);
-        }
-    */
-
-    rightRotation: (src: any[][]) => {
-      let srcRow = src.length;
-      let srcCol = src[0].length;
-      let dst = MapMethod.createArray(srcCol, srcRow, -1);
-      src.forEach((i, row) => {
-        i.forEach((j, col) => {
-          dst[col][srcRow - 1 - row] = j;
-        });
-      });
-      return dst;
-    },
-
-    leftRotation: (src: any[][]) => {
-      let srcRow = src.length;
-      let srcCol = src[0].length;
-      let dst = MapMethod.createArray(srcCol, srcRow, -1);
-      src.forEach((i, row) => {
-        i.forEach((j, col) => {
-          dst[srcCol - 1 - col][row] = j;
-        });
-      });
-      return dst;
-    },
-
-    createArray: (row, col, fill) => Array(row).fill(fill).map((i) => Array(col).fill(fill))
-
+  const toAscii=(src: any[][], head = "")=>{
+    const blank = " ";
+    return src.reduce((acc_row, cur_row) => {
+      let hoge = cur_row.map((x) => (x === "" ? blank : String(x))).join("");
+      return `${acc_row}${head}${hoge}\r\n`;
+    }, "");
   };
 
-  return MapMethod;
+  const rotation =(src: any[][], rot = 1)=> {
+    if (rot > 0) {
+      return rotation( rightRotation(src), rot - 1);
+    } else if (rot < 0) {
+      return rotation( leftRotation(src), rot + 1);
+    } else {
+      return src;
+    }
+  };
+
+  const rightRotation = (src: any[][]) => {
+    let srcRow = src.length;
+    let srcCol = src[0].length;
+    let dst = createArray(srcCol, srcRow, -1);
+    src.forEach((i, row) => {
+      i.forEach((j, col) => {
+        dst[col][srcRow - 1 - row] = j;
+      });
+    });
+    return dst;
+  };
+
+  const leftRotation = (src: any[][]) => {
+    let srcRow = src.length;
+    let srcCol = src[0].length;
+    let dst = createArray(srcCol, srcRow, -1);
+    src.forEach((i, row) => {
+      i.forEach((j, col) => {
+        dst[srcCol - 1 - col][row] = j;
+      });
+    });
+    return dst;
+  };
+
+  const createArray = (row, col, fill) =>
+    Array(row)
+      .fill(fill)
+      .map((i) => Array(col).fill(fill));
+
+  return {
+    bottom, left, top, right,
+    toAscii,
+    rotation, rightRotation, leftRotation
+  };
+
 });
 
